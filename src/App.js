dd65160d00982d744117894844ff1220a4a6cbc8
Guilherme Martins <gui.martins.soares@gmail.com>
2024-10-03 20:04:31 -0300

    Added style to page and buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Button from './Components/Button';
+import Input from './Components/Input';
+import { Container, Content, Row, Column } from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <Button title={"Botão!"}/>
-    </div>
+    <Container>
+      <Content>
+        <Input title={"Botão do Input!"}/>
+        <Row>
+          <Button label="7"/>
+          <Button label="8"/>
+          <Button label="9"/>
+          <Button label="="/>
+        </Row>
+        <Row>
+          <Button label="4"/>
+          <Button label="5"/>
+          <Button label="6"/>
+          <Button label="+"/>
+        </Row>
+        <Row>
+          <Button label="1"/>
+          <Button label="2"/>
+          <Button label="3"/>
+          <Button label="-"/>
+        </Row>
+      </Content>
+    </Container>
   );
 }
 
